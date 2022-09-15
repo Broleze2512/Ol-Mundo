c4bb79859769c5f76412f5eb7bd4768e3054ea4d
Broleze2512 <113644885+Broleze2512@users.noreply.github.com>
2022-09-15 19:48:01 -0300

    Criado o método para falar Olá Mundo

--- OlaMundo/src/olamundo/OlaMundo.java
+++ OlaMundo/src/olamundo/OlaMundo.java
@@ -6,7 +6,10 @@ public class OlaMundo {
 
     
     public static void main(String[] args) {
+           EscreverOlaMundo();
+    }
+    public static void EscreverOlaMundo(){
+            System.out.println("Olá Mundo");
        
     }
-    
 }
